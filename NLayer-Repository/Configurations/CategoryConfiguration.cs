311cf971c381f9c36bd6ded304614d794ae72c02
Kutluhan <kutluhan.ozbakan21@gmail.com>
2023-11-11 15:56:06 +0300

    AppDbContext & Configurations

---
+++ NLayer-Repository/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayer.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer_Repository.Configurations
+{
+    internal class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {            
+        //Fluent API
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.ToTable("Categories");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).UseIdentityColumn();
+            builder.Property(x=> x.Name).IsRequired().HasMaxLength(50);
+
+        }
+    }
+}
